b37b430766555829d80e14d26e53f2e9156c9306
ronald <ottoniel.21@outlook.es>
2020-09-22 20:33:12 -0600

    Metodos para convertir AnimalEntity a JSON

---
+++ src/main/java/gt/com/umg/AS2P2/controller/AnimalController.java
@@ -0,0 +1,33 @@
+package gt.com.umg.AS2P2.controller;
+
+import com.google.gson.Gson;
+import gt.com.umg.AS2P2.dao.AnimalDao;
+import gt.com.umg.AS2P2.entity.AnimalEntity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AnimalController {
+
+    public List <String> convertAllAnimal(String id){
+        List <String> json = new LinkedList<>();
+        Gson gson = new Gson();
+        AnimalDao animal = new AnimalDao();
+        List<AnimalEntity> listAnimal = animal.readAllAnimals(id);
+        for(int i =0; i <listAnimal.size();i++) {
+            json.add(gson.toJson(listAnimal.get(i)));
+        }
+        return json;
+    }
+    public List <String> convertAnimal(String id){
+        List <String> json = new LinkedList<>();
+        Gson gson = new Gson();
+        AnimalDao animal = new AnimalDao();
+        List<AnimalEntity> listAnimal = animal.select(id);
+        for(int i =0; i <listAnimal.size();i++) {
+            json.add(gson.toJson(listAnimal.get(i)));
+        }
+        return json;
+    }
+
+}
