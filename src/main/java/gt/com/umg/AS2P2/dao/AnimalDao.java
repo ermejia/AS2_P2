d498b6445a3b5c9efd9c58cf65b8cbea49e3c15f
oscarrl98 <oscar_rlp@hotmail.es>
2020-09-23 21:45:17 -0600

    Se han implementado los metodos Create,Delete y update

--- src/main/java/gt/com/umg/AS2P2/dao/AnimalDao.java
+++ src/main/java/gt/com/umg/AS2P2/dao/AnimalDao.java
@@ -14,17 +14,20 @@ import java.util.List;
 
 public class AnimalDao implements ICrud {
 
+
     @Override
     public  List<AnimalEntity> select(String id) {
         List<AnimalEntity> listAnimals =new LinkedList<>();
-        AnimalEntity animal = new AnimalEntity();
+        AnimalEntity animal;
+        Connection conexion = null;
         try{
             ConectionService con= ConectionService.getInstance();
-            Connection conexion = con.getConnection();
+            conexion = con.getConnection();
             PreparedStatement statement = conexion.prepareStatement("SELECT * FROM Animal Where IdArea ="+id);
             ResultSet consulta=statement.executeQuery();
 
             while (consulta.next()) {
+                animal = new AnimalEntity();
                 animal.setIdAnimal(consulta.getInt("IdAnimal"));
                 animal.setIdArea(consulta.getInt("IdArea"));
                 animal.setName(consulta.getString("Name"));
@@ -39,13 +42,16 @@ public class AnimalDao implements ICrud {
 
                 listAnimals.add(animal);
             }
-            conexion.close();
             consulta.close();
         }catch (SQLException e){
             System.err.println("ERROR FATAL! ");
             System.err.println(e.getMessage());
         }finally {
-
+            try {
+                conexion.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
         return listAnimals;
     }
@@ -53,14 +59,15 @@ public class AnimalDao implements ICrud {
     @Override
     public List<AnimalEntity> readAllAnimals(String id) {
         List<AnimalEntity> listAnimals =new LinkedList<>();
-        AnimalEntity animal = new AnimalEntity();
+        AnimalEntity animal;
+        Connection conexion = null;
         try{
             ConectionService con= ConectionService.getInstance();
-            Connection conexion = con.getConnection();
+            conexion = con.getConnection();
             PreparedStatement statement = conexion.prepareStatement("SELECT * FROM Animal WHERE IdArea = "+id);
             ResultSet consulta=statement.executeQuery();
             while (consulta.next()) {
-                animal.setIdAnimal(consulta.getInt("IdAnimal"));
+                animal = new AnimalEntity();
                 animal.setIdArea(consulta.getInt("IdArea"));
                 animal.setName(consulta.getString("Name"));
                 animal.setAge(consulta.getInt("Age"));
@@ -81,7 +88,11 @@ public class AnimalDao implements ICrud {
             System.err.println("ERROR FATAL! ");
             System.err.println(e.getMessage());
         }finally {
-
+            try {
+                conexion.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
         return listAnimals;
     }
@@ -89,16 +100,102 @@ public class AnimalDao implements ICrud {
 
     @Override
     public void delete(String id) {
+        AnimalEntity animal = new AnimalEntity();
+        Connection conexion = null;
+        try{
+            ConectionService con= ConectionService.getInstance();
+            conexion = con.getConnection();
+            PreparedStatement parametro;
+            String query = "DELETE FROM Animal WHERE IdAnimal="+id;
+            parametro = conexion.prepareStatement(query);
+            parametro.executeUpdate();
+            conexion.close();
+
+        }catch (SQLException e){
+            System.err.println("ERROR FATAL! ");
+            System.err.println(e.getMessage());
+        }finally {
+            try {
+                conexion.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
 
     }
 
     @Override
     public void update(AnimalEntity animal) {
+    
+        AnimalEntity animal1 = new AnimalEntity();
+        Connection conexion = null;
+        try{
+            ConectionService con= ConectionService.getInstance();
+            conexion = con.getConnection();
+            PreparedStatement parametro;
+            String query = "UPDATE Animal set IdArea=?,Name=?,Age=?,Heigth=?,Weight=?,Color=?,Gender=?,Country=?,Extinction_Risk=?,Scientific_Name=? WHERE IdAnimal=?;";
+            parametro = conexion.prepareStatement(query);
+            parametro.setInt(1,animal.getIdArea());
+            parametro.setString(2,animal.getName());
+            parametro.setInt(3,animal.getAge());
+            parametro.setFloat(4,animal.getHeigth());
+            parametro.setFloat(5,animal.getWeight());
+            parametro.setString(6,animal.getColor());
+            parametro.setString(7,animal.getGender());
+            parametro.setString(8,animal.getCountry());
+            parametro.setString(9,animal.getExtinctionRisk());
+            parametro.setString(10,animal.getSciName());
+            parametro.setInt(11,animal.getIdAnimal());
+            parametro.executeUpdate();
+            conexion.close();
+
+        }catch (SQLException e){
+            System.err.println("ERROR FATAL! ");
+            System.err.println(e.getMessage());
+        }finally {
+            try {
+                conexion.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
 
     }
 
     @Override
     public void create(AnimalEntity animal) {
+        AnimalEntity animal1 = new AnimalEntity();
+        Connection conexion = null;
+        try{
+            ConectionService con= ConectionService.getInstance();
+            conexion = con.getConnection();
+            PreparedStatement parametro;
+            String query = "INSERT INTO Animal(IdArea,Name,Age,Heigth,Weight,Color,Gender,Country,Extinction_Risk,Scientific_Name) VALUES(?,?,?,?,?,?,?,?,?,?);";
+            parametro = conexion.prepareStatement(query);
+            parametro.setInt(1,animal.getIdArea());
+            parametro.setString(2,animal.getName());
+            parametro.setInt(3,animal.getAge());
+            parametro.setFloat(4,animal.getHeigth());
+            parametro.setFloat(5,animal.getWeight());
+            parametro.setString(6,animal.getColor());
+            parametro.setString(7,animal.getGender());
+            parametro.setString(8,animal.getCountry());
+            parametro.setString(9,animal.getExtinctionRisk());
+            parametro.setString(10,animal.getSciName());
+            parametro.executeUpdate();
+            conexion.close();
+
+        }catch (SQLException e){
+            System.err.println("ERROR FATAL! ");
+            System.err.println(e.getMessage());
+        }finally {
+            try {
+                conexion.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
 
     }
 }
