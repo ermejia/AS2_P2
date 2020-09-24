7c282013ce87f4ca77c12fe8258035cdc1a3cfcb
ronald <ottoniel.21@outlook.es>
2020-09-24 17:31:23 -0600

    Correccion en el js

--- FrontEndAS2P2/js/home.js
+++
@@ -1,7 +0,0 @@
-axios.get('http://localhost:7001/AS2P2-1.0-SNAPSHOT/animalServlet')
-    .then(response => {
-        console.log(response.data)
-    })
-    .catch(error => {
-        console.error(error)
-    });
